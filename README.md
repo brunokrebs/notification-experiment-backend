35f7360e1e713fc53d75698234e3e2a28e9b1960
Bruno Krebs <bruno.krebs@auth0.com>
2021-03-22 15:39:30 -0300

    my little incomplete mess;

--- README.md
+++ README.md
@@ -5,8 +5,15 @@ Backend:
 ```bash
 docker build -t notification-experiment-backend .
 
-docker run -e "INSTANCE_ID=First instance" -p 8081:3000 -d notification-experiment-backend
-docker run -e "INSTANCE_ID=Second instance" -p 8082:3000 -d notification-experiment-backend
+docker run -e "INSTANCE_ID=First instance" \
+  -p 9501:3000 \
+   --name notification-experiment-backend-1 \
+  -d notification-experiment-backend
+
+docker run -e "INSTANCE_ID=Second instance" \
+  -p 9502:3000 \
+   --name notification-experiment-backend-2 \
+  -d notification-experiment-backend
 ```
 
 NGINX load balancer:
@@ -16,5 +23,35 @@ cd nginx-load-balancer
 
 docker build -t nginx-load-balancer .
 
-docker run -p 8080:80 -d nginx-load-balancer
+docker run \
+  -p 9500:80 \
+  --name notification-experiment-load-balancer \
+  -d nginx-load-balancer
+```
+
+## Localstack SQS
+
+References:
+
+- https://github.com/localstack/localstack
+- https://www.linkedin.com/pulse/using-localstack-test-sqs-sns-fabio-palumbo/?articleId=6653638519531073536
+
+```bash
+export SERVICES=sqs,sns
+
+localstack start
+```
+
+Create queue:
+
+```bash
+aws --endpoint-url=http://localhost:4566 sqs create-queue --region us-east-1 --queue-name notification-experiment-queue
+```
+
+Output:
+
+```
+{
+    "QueueUrl": "http://localhost:4566/000000000000/notification-experiment-queue"
+}
 ```
--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,9 +1,78 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { Consumer } from 'sqs-consumer';
+import { config, SQS } from 'aws-sdk';
 
 @Controller('messages')
 export class MessagesController {
+  private sqsClient = null;
+  private queueUrl =
+    'http://localhost:4566/000000000000/notification-experiment-queue';
+
+  constructor() {
+    config.update({
+      region: 'us-east-1',
+      accessKeyId: 'foo',
+      secretAccessKey: 'bar',
+    });
+
+    const app = Consumer.create({
+      queueUrl: this.queueUrl,
+      handleMessage: async (message) => {
+        console.log(
+          `The following message was handled by ${process.env.INSTANCE_ID}:`,
+        );
+        console.log(message.Body);
+        console.log('----------------------------------');
+        setTimeout(() => {
+          this.sendMessage();
+        }, 5000);
+      },
+      sqs: new SQS({
+        apiVersion: '2012-11-05',
+      }),
+    });
+
+    app.on('error', (err) => {
+      console.error(err.message);
+    });
+
+    app.on('processing_error', (err) => {
+      console.error(err.message);
+    });
+
+    app.start();
+
+    this.sqsClient = new SQS({
+      apiVersion: '2012-11-05',
+    });
+  }
+
   @Get()
   public getMessage(): string {
     return `I'm alive and well, thanks: ${process.env.INSTANCE_ID}.`;
+  }
+
+  private sendMessage() {
+    const params = {
+      MessageBody: `Message timestamp ${new Date().toISOString()}`,
+      QueueUrl: this.queueUrl,
+      MessageAttributes: {
+        SomeAttr: {
+          DataType: 'String',
+          StringValue: 'cool beans',
+        },
+      },
+    };
+
+    this.sqsClient.sendMessage(params, (err, result) => {
+      if (err) return console.log(err);
+      console.log('message sent: ' + result.MessageId);
+      console.log('----------------------------------');
+    });
+  }
+
+  @Post()
+  public addMessage(): void {
+    this.sendMessage();
   }
 }
